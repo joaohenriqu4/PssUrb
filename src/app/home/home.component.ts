7dcf5d236850f644addca486d4ebadcc7617eaff
João Henrique <joaohenriqu4@gmail.com>
2019-01-24 22:54:01 -0300

    promise

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Oferta } from '../shared/oferta.model'
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
@@ -16,8 +16,14 @@ export class HomeComponent implements OnInit {
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    this.ofertas = this.ofertasService.getOfertas()
-    console.log(this.ofertas)
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      })
+      .catch((param: any) => {
+        console.log(param)
+      })
+
   }
 
 }
